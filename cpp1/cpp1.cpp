e5f08e3d67e649b7aba318b688d968f865050b4a
Mohamed Eleraqi <81626129+3iraqi@users.noreply.github.com>
2023-05-14 07:21:38 +0300

    Add files via upload

---
+++ cpp1/cpp1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init() { cin.tie(0); cin.sync_with_stdio(0); }
+int main() {
+    init();
+       
+    int n; cin >> n;
+    int arr[n]{ n };
+
+
+        return 0;
+    }
+
+
+    
